8b8ca449a236041dae2b3bd7377480100d6dd9ed
darkraven92 <36090181+darkraven92@users.noreply.github.com>
2023-02-23 20:34:31 +0100

    Update Lab1.java

--- src/Laboration1/Lab1.java
+++ src/Laboration1/Lab1.java
@@ -1,16 +1,25 @@
-package Laboration1;
+public class LifeLength {
+    public static int f1(int a0) {
+        if (a0 == 1) {
+            return 1;
+        } else if (a0 % 2 == 0) {
+            return a0 / 2;
+        } else {
+            return 3 * a0 + 1;
+        }
+    }
 
-public class Lab1 {
-
-	public static void main(String[] args){
-		
-		System.out.println("Hello World");
-		
-		
-	
-	}
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please provide an argument");
+            return;
+        }
+        int a0 = Integer.parseInt(args[0]);
+        int current = a0;
+        while (current != 1) {
+            System.out.println(current);
+            current = f1(current);
+        }
+        System.out.println(current);
+    }
 }
-	
-	
-
-
